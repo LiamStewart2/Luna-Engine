28a9ad636d42487a7f6916125ee25c8a75723140
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-05-30 06:20:15 +0100

    now loading all data into the 3 buffers

--- Luna-Engine/src/AssetLoader.cpp
+++ Luna-Engine/src/AssetLoader.cpp
@@ -3,27 +3,46 @@
 void AssetLoader::LoadMesh(Mesh& mesh, const char* filepath)
 {
 	std::ifstream file(filepath); std::string line;
-
-	Vertex currentValues = Vertex();
+	if (!file) {
+		std::cerr << "Cannot open file: " << filepath << std::endl;
+	}
 
 	while (std::getline(file, line))
 	{
-		// Mesh Name
-		if (line[0] == 'o')
-			mesh.name =  line.substr(2, line.size() - 2);
+		std::istringstream ss(line);
+		std::string prefix;
+		ss >> prefix;
 
-		if (line[0] == 'v')
+		if(prefix == "o")
+			ss >> mesh.name;
+
+		else if (prefix == "v") 
 		{
-			// Vertex Position
-			if (line[1] == ' ')
-			{
-				int current = 1;
-				for (int i = 0; i < 3; i++)
-				{
-					
-				}
-				std::cout << " : " << currentValues.position.x << ", " << currentValues.position.y << ", " << currentValues.position.z << "\n";
-			}
+			mesh.vertexPositions.push_back(glm::vec3());
+
+			ss >> mesh.vertexPositions[mesh.vertexPositions.size() - 1].x;
+			ss >> mesh.vertexPositions[mesh.vertexPositions.size() - 1].y;
+			ss >> mesh.vertexPositions[mesh.vertexPositions.size() - 1].z;
+		}
+		else if (prefix == "vn")
+		{
+			mesh.vertexNormals.push_back(glm::vec3());
+
+			ss >> mesh.vertexNormals[mesh.vertexNormals.size() - 1].x;
+			ss >> mesh.vertexNormals[mesh.vertexNormals.size() - 1].y;
+			ss >> mesh.vertexNormals[mesh.vertexNormals.size() - 1].z;
+		}
+		else if (prefix == "vt")
+		{
+			mesh.vertexTextureCoords.push_back(glm::vec2());
+
+			ss >> mesh.vertexTextureCoords[mesh.vertexTextureCoords.size() - 1].x;
+			ss >> mesh.vertexTextureCoords[mesh.vertexTextureCoords.size() - 1].y;
+		}
+
+		else if (prefix == "f")
+		{
+
 		}
 	}
 
