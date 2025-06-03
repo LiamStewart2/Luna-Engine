80624286830b358790f081ee866cbee96082c4f6
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-06-03 02:39:35 +0100

    have a monkey renderering

--- Luna-Engine/Assets/Shaders/Shader/shader.fs
+++ Luna-Engine/Assets/Shaders/Shader/shader.fs
@@ -1,7 +1,7 @@
 #version 460 core
 
-layout(location = 0) in vec3 v_vertexTextureCoordinate;
-layout(location = 1) in vec2 v_vertexNormal;
+layout(location = 0) in vec2 v_vertexTextureCoordinate;
+layout(location = 1) in vec3 v_vertexNormal;
 
 out vec4 color;
 
--- Luna-Engine/src/Mesh.cpp
+++ Luna-Engine/src/Mesh.cpp
@@ -2,28 +2,25 @@
 
 void Mesh::BuildMesh()
 {
-	if (!VAO)
-	{
-		glGenVertexArrays(1, &VAO);
-		glGenBuffers(1, &VBO);
-		glGenBuffers(1, &EBO);
-	}
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*) 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Position));
 
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)offsetof(Vertex, TextureCoordiante));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TextureCoordiante));
 
 	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)offsetof(Vertex, Normal));
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
 
 	glBindVertexArray(0);
 }
