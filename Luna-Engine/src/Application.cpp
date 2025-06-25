bf5f0d9e1dfd8acae2753b46ce4b63739f25d3f0
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-06-25 17:14:00 +0100

    changed colour of background

--- Luna-Engine/src/Application.cpp
+++ Luna-Engine/src/Application.cpp
@@ -101,7 +101,7 @@ void Application::Update()
 
 void Application::Render()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.7f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	scene.Render(&renderer);
