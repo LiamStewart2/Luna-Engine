dcca04295e37eafaa6b575827223adb211fc8b01
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-07-06 02:31:05 +0100

    removed odd shader code making terrible artifacts

--- Luna-Engine/Assets/Shaders/Shader/shader.fs
+++ Luna-Engine/Assets/Shaders/Shader/shader.fs
@@ -96,7 +96,7 @@ float ShadowCalculation(vec3 fragmentPositionWorldSpace)
 		for(int y = -1; y <= 1; ++y)
 		{
 			vec2 coordSample = vec2(x, y);
-			vec2 offset = vec2(rand(v_vertexTextureCoordinate.xy) * (cascadeCount - layer) / 2, rand(v_vertexTextureCoordinate.xy + vec2(1, 1)) * (cascadeCount - layer) / 2);
+			vec2 offset = vec2(rand(v_vertexTextureCoordinate.xy), rand(v_vertexTextureCoordinate.xy + vec2(1, 1)));
 
 			vec2 sampleOffset = coordSample + offset;
 
