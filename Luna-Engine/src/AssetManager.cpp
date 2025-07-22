7a979e5d32f85b8f51e37f0723c7b75103a746d6
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-07-22 01:27:40 +0100

    built AssetManager functions

--- Luna-Engine/src/AssetManager.cpp
+++ Luna-Engine/src/AssetManager.cpp
@@ -1,1 +1,38 @@
 #include "AssetManager.h"
+
+AssetManager::AssetManager()
+{
+}
+
+AssetManager::~AssetManager()
+{
+}
+
+std::shared_ptr<Texture> AssetManager::GetTexture(std::string filepath)
+{
+	if(m_Textures.find(filepath) == m_Textures.end())
+	{
+		
+		std::shared_ptr<Texture> texture = m_Textures[filepath] = std::make_shared<Texture>();
+		AssetLoader::LoadTexture(texture, filepath.c_str());
+	}
+	return m_Textures[filepath];
+}
+
+std::shared_ptr<Mesh> AssetManager::GetMesh(std::string filepath)
+{
+	if (m_Meshes.find(filepath) == m_Meshes.end())
+	{
+
+		std::shared_ptr<Mesh> mesh = m_Meshes[filepath] = std::make_shared<Mesh>();
+		AssetLoader::LoadMeshOBJ(mesh, filepath.c_str());
+	}
+	return m_Meshes[filepath];
+}
+
+std::shared_ptr<Shader> AssetManager::GetShader(std::string filepath)
+{
+	if (m_Shaders.find(filepath) == m_Shaders.end())
+		m_Shaders[filepath] = std::make_shared<Shader>(filepath + std::string("/shader.vs"), filepath + std::string("/shader.fs"), filepath + std::string("/shader.gs"));
+	return m_Shaders[filepath];
+}
--- Luna-Engine/src/AssetManager.h
+++ Luna-Engine/src/AssetManager.h
@@ -16,18 +16,13 @@ public:
 	AssetManager();
 	~AssetManager();
 
-	void LoadTexture(std::string filepath);
-	void LoadMesh(std::string filepath);
-	void LoadShader(std::string filepath);
-
 	std::shared_ptr<Texture> GetTexture(std::string filepath);
 	std::shared_ptr<Mesh> GetMesh(std::string filepath);
 	std::shared_ptr<Shader> GetShader(std::string filepath);
 
 private:
 	std::unordered_map<std::string, std::shared_ptr<Texture>> m_Textures;
-	std::unordered_map<std::string, std::shared_ptr<Mesh>> m_Mesh;
-	std::unordered_map<std::string, std::shared_ptr<Shader>> m_Shader;
-
+	std::unordered_map<std::string, std::shared_ptr<Mesh>> m_Meshes;
+	std::unordered_map<std::string, std::shared_ptr<Shader>> m_Shaders;
 };
 
