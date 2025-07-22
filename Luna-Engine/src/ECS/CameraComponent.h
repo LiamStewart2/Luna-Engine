c00781995cd66b5ab8e38a62ee2eadb645f881bf
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-07-22 11:44:26 +0100

    added CameraComponent class

---
+++ Luna-Engine/src/ECS/CameraComponent.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "GLM/glm.hpp"
+
+#include "Component.h"
+
+class CameraComponent : public Component
+{
+	glm::vec3 forward, up;
+	float m_NearPlane = 0.1f, m_FarPlane = 250.0f;
+	float pov = 90.0f;
+};
