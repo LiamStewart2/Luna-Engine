41452c8925840fafd02cd9740513cf44a3247e12
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-07-27 20:34:54 +0100

    finished camera component

--- Luna-Engine/src/ECS/CameraComponent.h
+++ Luna-Engine/src/ECS/CameraComponent.h
@@ -6,7 +6,9 @@
 
 class CameraComponent : public Component
 {
-	glm::vec3 up;
-	float m_NearPlane = 0.1f, m_FarPlane = 250.0f;
-	float pov = 90.0f;
+	CameraComponent(unsigned int _gameObject = 0, glm::vec3 _up = glm::vec3(0), bool _renderToScreen = true) :
+		Component(_gameObject), m_Up(_up), m_RenderToScreen(_renderToScreen) {}
+
+	glm::vec3 m_Up;
+	bool m_RenderToScreen;
 };
