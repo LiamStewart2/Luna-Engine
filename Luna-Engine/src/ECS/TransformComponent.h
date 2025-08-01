6b19a4029a5eea53d8c3e38afd168bdda2ea0f85
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-08-01 13:15:38 +0100

    started work moving the camera component around

--- Luna-Engine/src/ECS/TransformComponent.h
+++ Luna-Engine/src/ECS/TransformComponent.h
@@ -7,8 +7,9 @@
 struct Transform : public Component
 {
 	Transform(unsigned int _gameObject = 0, glm::vec3 _position = glm::vec3(1), glm::vec3 _rotation = glm::vec3(0), glm::vec3 _scale = glm::vec3(1)) : 
-		Component(_gameObject), position(_position), rotation(_rotation), scale(_scale), transformMatrix(glm::mat4(1)) {}
+		Component(_gameObject), position(_position), rotation(_rotation), scale(_scale), transformMatrix(glm::mat4(1)), forward(glm::vec3(0)), up(glm::vec3(0, 1, 0)) {}
 	
 	glm::vec3 position, rotation, scale;
 	glm::mat4 transformMatrix;
+	glm::vec3 forward, up;
 };
