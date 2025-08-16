365acced7d337c526b92efc6c711f1d7c593f016
Liam <liam.oliver.stewart@gmail.com>
2025-08-16 03:35:03 +0100

    fixed scene graph issues

--- Luna-Engine/src/SceneGraph.h
+++ Luna-Engine/src/SceneGraph.h
@@ -8,13 +8,12 @@ public:
 	SceneGraphNode(unsigned int gameObject);
 	
 	// node - the node which will own the game object
-	SceneGraphNode* GetNode(unsigned int gameObject, SceneGraphNode* node);
+	SceneGraphNode* GetNode(unsigned int gameObject, SceneGraphNode* parentNode);
 
 	// ParentNode - the node which will own the game object
 	void InsertNode(unsigned int gameObject, SceneGraphNode* parentNode = nullptr);
-	// parentNode - the node which owns the Scene Graph Node you wish to remove
-	void RemoveNode(unsigned int gameObject, SceneGraphNode* parentNode = nullptr);
-	void KillBranch(unsigned int gameObject, SceneGraphNode* parentNode = nullptr);
+	// node - the node which will be removed, if nullptr, the current node will be removed
+	void RemoveNode(unsigned int gameObject, SceneGraphNode* node = nullptr);
 
 	std::vector<SceneGraphNode*>* getNodes() { return &m_Nodes; }
 	unsigned int GetGameObject() {return m_GameObject;}
