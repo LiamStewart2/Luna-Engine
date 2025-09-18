c62bd66da813cd86dece49bf39c2dee5f0d7db26
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-09-18 14:33:15 +0100

    added python

--- Luna-Editor/src/main.cpp
+++ Luna-Editor/src/main.cpp
@@ -1,7 +1,15 @@
 #include "Application.h"
 
+#include <Python.h>
+
 int main()
 {
+	Py_Initialize();
+
+	PyRun_SimpleString("print(7489217 + 4781)");
+
+	Py_Finalize();
+
 	Application application;
 	return 0;
 }
